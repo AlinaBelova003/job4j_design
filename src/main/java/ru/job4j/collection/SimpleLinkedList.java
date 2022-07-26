00cb0619410b53aef3d1253ce4ea26ab54f5a03d
alina <alinka_belova_2003@mail.ru>
2022-07-26 17:11:07 +0300

    2. Создать контейнер на базе связанного списка [#159 #325628]

--- src/main/java/ru/job4j/collection/SimpleLinkedList.java
+++ src/main/java/ru/job4j/collection/SimpleLinkedList.java
@@ -2,7 +2,9 @@ package ru.job4j.collection;
 
 import org.w3c.dom.Node;
 
+import java.util.ConcurrentModificationException;
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 import java.util.Objects;
 
 
@@ -24,6 +26,12 @@ public class SimpleLinkedList<E> implements LinkedList<E> {
     private static class Node<E> {
         E data;
         Node<E> next;
+
+
+        public Node(E data, Node<E> next) {
+            this.next = next;
+            this.data = data;
+        }
     }
 
     public boolean isEmpty() {
@@ -36,12 +44,11 @@ public class SimpleLinkedList<E> implements LinkedList<E> {
      * Если список пуст, то голова и хвост указывают на новый созданный элемент.
      * Если элемент не пуст, то к хвосту задаём ссылку на новый элемент. И этот элемент становится новым хвостом.
      * Новый элемент всегда находиться в хвосте и увеличивает размер и счетчик.
-     *
-     * @param value
+   
      */
     @Override
     public void add(E value) {
-        Node<E> newList = new Node<>();
+        Node<E> newList = new Node<>(value, null);
         newList.data = value;
         if (isEmpty()) {
             newList = head;
@@ -54,18 +61,50 @@ public class SimpleLinkedList<E> implements LinkedList<E> {
         modCount++;
     }
 
+    /**
+     * Метод возвращает элемент, расположенный по указанному индексу.
+     * Проверяем, что индекс находиться в пределах диапозона size.
+     * Элемент(result) находиться в голове списка
+     * Проходим до индекса. Двигая голову.
+     * @return значение(данные пользователя) хранившиеся в узле
+     */
     @Override
     public E get(int index) {
         Objects.checkIndex(index, size);
-        Node<E> result = null;
+        Node<E> result = head;
         for (int i = 0; i < index; i++) {
             result = head.next;
         }
-        return null;
+        return result.data;
     }
 
     @Override
     public Iterator<E> iterator() {
-        return null;
+        return new Iterator<E>() {
+            final int expectedModCount = modCount;
+            private SimpleLinkedList.Node<E> index = head;
+
+            @Override
+            public boolean hasNext() {
+                if (expectedModCount != modCount) {
+                    throw new ConcurrentModificationException();
+                }
+                return index != null;
+            }
+
+            /**
+             * Элемент равен голове списке со всеми данными. Голова движется по списку.
+             * @return голова списка
+             */
+            @Override
+            public E next() {
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                E element = index.data;
+                index = index.next;
+                return element;
+            }
+        };
     }
 }
---
+++ src/test/java/ru/job4j/collection/SimpleLinkedListTest.java
@@ -0,0 +1,96 @@
+package ru.job4j.collection;
+
+import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+
+import java.util.Iterator;
+
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class SimpleLinkedListTest {
+
+    private LinkedList<Integer> list;
+
+    @BeforeEach
+    public void initData() {
+        list = new SimpleLinkedList<>();
+        list.add(1);
+        list.add(2);
+    }
+
+    @Test
+   public void checkIteratorSimple() {
+        assertThat(list).hasSize(2);
+        list.add(3);
+        list.add(4);
+        assertThat(list).hasSize(4);
+    }
+
+    @Test
+   public void whenAddAndGet() {
+        list.add(3);
+        list.add(4);
+        assertThat(list.get(0)).isEqualTo(1);
+        assertThat(list.get(1)).isEqualTo(2);
+        assertThat(list.get(2)).isEqualTo(3);
+        assertThat(list.get(3)).isEqualTo(4);
+    }
+
+    @Test
+   public void whenGetFromOutOfBoundThenExceptionThrown() {
+        assertThatThrownBy(() -> list.get(2))
+                .isInstanceOf(IndexOutOfBoundsException.class);
+    }
+
+    @Test
+    public void whenAddIterHasNextTrue() {
+        Iterator<Integer> it = list.iterator();
+        assertThat(it.hasNext()).isTrue();
+    }
+
+    @Test
+   public void whenAddIterNextOne() {
+        Iterator<Integer> it = list.iterator();
+        assertThat(it.next()).isEqualTo(1);
+    }
+
+    @Test
+    public void whenEmptyIterHashNextFalse() {
+        LinkedList<Integer> list = new SimpleLinkedList<>();
+        Iterator<Integer> it = list.iterator();
+        assertThat(it.hasNext()).isFalse();
+    }
+
+    @Test
+    void whenAddIterMultiHasNextTrue() {
+        Iterator<Integer> it = list.iterator();
+        assertThat(it.hasNext()).isTrue();
+        assertThat(it.hasNext()).isTrue();
+    }
+
+    @Test
+    void whenAddIterNextOneNextTwo() {
+        Iterator<Integer> it = list.iterator();
+        assertThat(it.next()).isEqualTo(1);
+        assertThat(it.next()).isEqualTo(2);
+    }
+
+    @Test
+    void whenGetIteratorTwiceThenEveryFromBegin() {
+        Iterator<Integer> first = list.iterator();
+        assertThat(first.hasNext()).isTrue();
+        assertThat(first.next()).isEqualTo(1);
+        assertThat(first.hasNext()).isTrue();
+        assertThat(first.next()).isEqualTo(2);
+        assertThat(first.hasNext()).isFalse();
+        Iterator<Integer> second = list.iterator();
+        assertThat(second.hasNext()).isTrue();
+        assertThat(second.next()).isEqualTo(1);
+        assertThat(second.hasNext()).isTrue();
+        assertThat(second.next()).isEqualTo(2);
+        assertThat(second.hasNext()).isFalse();
+    }
+
+}
