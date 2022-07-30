7b6300aa65fc05ce2624898350113c9c2cb40804
alina <alinka_belova_2003@mail.ru>
2022-07-30 20:33:57 +0300

    5. Очередь на двух стеках [#160 #326935]

--- src/main/java/ru/job4j/collection/SimpleQueue.java
+++ src/main/java/ru/job4j/collection/SimpleQueue.java
@@ -3,6 +3,8 @@ package ru.job4j.collection;
 import java.util.NoSuchElementException;
 
 public class SimpleQueue<T> {
+    private int countIn = 0;
+    private int countOut = 0;
     /**
      * out стопка из которой берем
      * in стопка куда перекладывааем, чтобы достать элемент out со дна.
@@ -13,13 +15,17 @@ public class SimpleQueue<T> {
 
     /**
      *  должен возвращать первое значение и удалять его из коллекции.
-     *  Перемещяем элемен в стопку и удаляем его из прошлой стопки
+     *  Перемещяем элемен в стопку in и удаляем его из прошлой стопки.
+     *  Счетчик стопки в которую мы добавили увеличивается
+     *  Из которой взяли уменьшается.
      */
     public T poll() {
-        if (out == null && in == null) {
+        if (out.isEmpty() && in.isEmpty()) {
             throw new NoSuchElementException();
         } else {
             in.push(out.pop());
+            countIn++;
+            countOut--;
         }
         return in.pop();
     }
@@ -29,5 +35,6 @@ public class SimpleQueue<T> {
      */
     public void push(T value) {
         in.push(value);
+        countIn++;
     }
   }
