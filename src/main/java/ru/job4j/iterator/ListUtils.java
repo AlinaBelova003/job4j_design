06fcf997986409fb7ab908cb5e43530e4f3c0ffe
alina <alinka_belova_2003@mail.ru>
2022-08-01 18:27:52 +0300

    7. ListIterator [#350217]

---
+++ src/main/java/ru/job4j/iterator/ListUtils.java
@@ -0,0 +1,74 @@
+package ru.job4j.iterator;
+
+import java.util.List;
+import java.util.ListIterator;
+import java.util.Objects;
+import java.util.function.Predicate;
+
+public class ListUtils {
+    /**
+     * будет вставлять значение перед индексом
+     */
+    public static <T> void addBefore(List<T> list, int index, T value) {
+        Objects.checkIndex(index, list.size());
+        ListIterator<T> iterator = list.listIterator();
+        while (iterator.hasNext()) {
+            if (iterator.nextIndex() == index) {
+                iterator.add(value);
+                break;
+            }
+            iterator.next();
+        }
+    }
+
+    /**
+     * будет вставлять значение после индекса
+     * int previousIndex(): возвращает индекс предыдущего элемента. Если такого нет, то возвращается число -1
+     */
+    public static <T> void addAfter(List<T> list, int index, T value) {
+        Objects.checkIndex(index, list.size());
+        ListIterator<T> iterator = list.listIterator();
+        while (iterator.hasNext()) {
+            if (iterator.previousIndex() == index) {
+                iterator.add(value);
+            }
+            iterator.next();
+        }
+    }
+
+    /**
+     * удаляет все элементы, которые удовлетворяют предикату.
+     */
+    public static <T> void removeIf(List<T> list, Predicate<T> filter) {
+        ListIterator<T> iterator = list.listIterator();
+        while (iterator.hasNext()) {
+            if (filter.test(iterator.next())) {
+                iterator.remove();
+            }
+        }
+    }
+
+    /**
+     * заменяет все элементы, которые удовлетворяют предикату;
+     */
+    public static <T> void replaceIf(List<T> list, Predicate<T> filter, T value) {
+        ListIterator<T> iterator = list.listIterator();
+        while (iterator.hasNext()) {
+            if (filter.test(iterator.next())) {
+                iterator.set(value);
+            }
+        }
+    }
+
+    /**
+     * удаляет из списка те элементы, которые есть в elements.
+     */
+    public static <T> void removeAll(List<T> list, List<T> elements) {
+        ListIterator<T> iterator = list.listIterator();
+        while (iterator.hasNext()) {
+            if (iterator.equals(elements)) {
+                iterator.remove();
+            }
+        }
+    }
+}
