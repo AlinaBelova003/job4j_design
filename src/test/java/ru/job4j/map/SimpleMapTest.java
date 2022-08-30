8c1a1254d206766097623b6e1aeb563d3487818f
alina <alinka_belova_2003@mail.ru>
2022-08-30 19:43:26 +0300

    8. Реализовать собственную структуру данных - HashMap [#1008 #332866]

--- src/test/java/ru/job4j/map/SimpleMapTest.java
+++ src/test/java/ru/job4j/map/SimpleMapTest.java
@@ -8,7 +8,6 @@ import static org.assertj.core.api.Assertions.*;
 
 class SimpleMapTest {
     private final SimpleMap<Integer, String> map = new SimpleMap<>();
-
     @BeforeEach
     void setUp() {
         map.put(1, "1");
