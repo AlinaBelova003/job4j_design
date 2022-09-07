cb7771d0e2f66c39632f1c0f2ad89363f56c51cb
alina <alinka_belova_2003@mail.ru>
2022-09-07 15:38:27 +0300

    0.2. FileInputStream [#4898]

--- src/main/java/ru/job4j/io/EvenNumberFile.java
+++ src/main/java/ru/job4j/io/EvenNumberFile.java
@@ -15,17 +15,23 @@ public class EvenNumberFile {
          * С помощью метода append () мы можем добавлять целые, двойные числа, символы - любой числовой тип.
          * Нам нужна переменная, в которую мы будет сохранять результаты прохода while - read
          * while - когда файл достигает конца, то метод read прекращяет чтение
-         * Но при этом при каждом проходе, мы переписываем четные значения элементов из int в char
+         * Но при этом при каждом проходе, мы переписываем значения элементов из int в char
+         * Получившийся текст можно разбить на строчки через метод String.split.
+         * Т.к line-String, а проверка на четность работает только с int, нужно преобразовать массив
          */
         try (FileInputStream in = new FileInputStream("even.txt")) {
             StringBuffer text = new StringBuffer();
             int read;
             while ((read = in.read()) != -1) {
-                if (read % 2 == 0) {
-                    text.append((char) read);
+                text.append((char) read);
+            }
+            String[] lines = text.toString().split(System.lineSeparator());
+            for (String line : lines) {
+                int i = Integer.parseInt(line);
+                if (i % 2 == 0) {
+                    System.out.println(i);
                 }
             }
-            System.out.println(text);
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
