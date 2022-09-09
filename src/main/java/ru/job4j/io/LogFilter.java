08e81891f263a8e38cf8f662ba7ee18d4a9f895a
alina <alinka_belova_2003@mail.ru>
2022-09-09 12:55:43 +0300

    0.3. BufferedReader. [#252489]

--- src/main/java/ru/job4j/io/LogFilter.java
+++ src/main/java/ru/job4j/io/LogFilter.java
@@ -7,21 +7,31 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Это наглядный пример использование шаблона декоратор. Один поток оборачивается в другой.
+ *
+ */
 public class LogFilter {
-
+    /**
+     * Фильтрует поток по проинципу - предпоследний элемент должен быть 404.
+     * Создали коллекцию, в которую будем записывать результат.
+     * Обернули reader(для чтения потоков символов) в in(для упрощения чтения из потоков ввода символов и повышения производительности)
+     * Проходим, сразу читая каждую строку и собирая всё в новый список.
+     * Мы не можем найти предпоследний элемент в коллекции, нам нужен массив элементов.
+     * line.split = разделяет строку на подстроки по кретерию "".
+     * Данные подстроки собираются методом в массив и представляют собой его возвращаемое значение.
+     * Находим предпоследний элемент заканчивающийся на 404 и добавляем этот элемент в ранее созданую коллекцию
+     */
     public List<String> filter(String file) {
         List<String> list = new ArrayList<>();
-        try (BufferedReader in = new BufferedReader(new FileReader(file))) {
-            String buffer;
-            while ((buffer = in.readLine()) != null) {
-                in.lines().forEach(System.out::println);
-            }
-            for (List<String> line : in.lines()) {
-                if ((line[in.readLine() - 2]).contains("404")) {
-                    list = line;
+        try (FileReader reader = new FileReader(file);
+             BufferedReader in = new BufferedReader(reader)) {
+            for (String line : in.lines().toList()) {
+                String[] array = line.split(" ");
+                if ((array[array.length - 2]).contains("404")) {
+                  list.add(line);
                 }
             }
-
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
