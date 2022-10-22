254041aa5afb8676b1667963b4a569d878f249b6
alina <alinka_belova_2003@mail.ru>
2022-10-22 15:38:33 +0300

    2. Анализ доступности сервера. [#859]

--- src/main/java/ru/job4j/io/Analysis.java
+++ src/main/java/ru/job4j/io/Analysis.java
@@ -1,44 +1,50 @@
 package ru.job4j.io;
 
 import java.io.*;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 public class Analysis {
     /**
      * Находит диапозоны, когда сервер не работал
-     * @param sours путь к файлу с диапозонами
+     *
+     * @param sours  путь к файлу с диапозонами
      * @param target файл с результатоми анализа
      */
     public void unavailable(String sours, String target) {
         try (BufferedReader reader = new BufferedReader(new FileReader(sours))) {
             List<String> list = reader.lines().toList();
-            writeTarget(target);
+            validate(target, list);
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }
-
-    }
-
-    /**
-     * На основе метода validate() записывает подходящие результаты в файл
-     */
-    public void writeTarget(String target) {
-        try (PrintWriter print = new PrintWriter(new FileOutputStream(target))) {
-            validate(target);
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
-
     }
 
     /**
      * Здесь нужно написать условия при которых диапозоны будут записаны
      */
-    private static void validate(String target) {
-
+    private static void validate(String target, List<String> list) {
+        try (PrintWriter print = new PrintWriter(new FileOutputStream(target))) {
+            boolean status = true;
+            for (String line : list) {
+                if (line.startsWith("400") || line.startsWith("500") && status == true) {
+                    line = target;
+                    print.write(target);
+                    status = false;
+                } else if (line.startsWith("200") || line.startsWith("300") && status == false) {
+                  line = target;
+                  print.write(target);
+                  status = true;
+                }
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
     }
+
 
     public static void main(String[] args) {
         try (PrintWriter out = new PrintWriter(new FileOutputStream("unavailable.csv"))) {
