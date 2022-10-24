4bc6fd91fef3bad1e93e80c5e4818a84067d14ab
alina <alinka_belova_2003@mail.ru>
2022-10-24 17:57:10 +0300

    4.0. File [#252491]

---
+++ src/main/java/ru/job4j/io/Dir.java
@@ -0,0 +1,24 @@
+package ru.job4j.io;
+
+import java.io.File;
+
+public class Dir {
+    public static void main(String[] args) {
+        File file = new File("c:\\projects");
+
+        if (!file.exists()) {
+            throw new IllegalArgumentException(String.format("Not exist %s", file.getAbsolutePath()));
+        }
+
+        if (!file.isDirectory()) {
+            throw new IllegalArgumentException(String.format("Not directory %s", file.getAbsoluteFile()));
+        }
+
+        System.out.println(String.format("size : %s", file.getTotalSpace()));
+        System.out.println(String.format("name : %s", file.getName()));
+        System.out.println(String.format("length : %s", file.length()));
+        for (File subfile : file.listFiles()) {
+            System.out.println(subfile.getAbsoluteFile());
+        }
+    }
+}
