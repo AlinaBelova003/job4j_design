4c845b86222909e0cbabc863434cafda41f37cb0
alina <alinka_belova_2003@mail.ru>
2022-11-04 10:25:12 +0300

    5. Валидация параметров запуска. [#246865 #357152]

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -1,5 +1,6 @@
 package ru.job4j.io;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.*;
 import java.util.List;
@@ -7,18 +8,36 @@ import java.util.function.Predicate;
 
 public class Search {
     public static void main(String[] args) throws IOException {
-       Path start = Paths.get("C:\\Users\\alina\\OneDrive\\Рабочий стол\\Потоки ввода -вывода");
-       String format = ".docx";
-       validate(start, format);
-       search(start, p -> p.toFile().getName().endsWith(".docx")).forEach(System.out::println);
+        validate(args);
+       Path start = Paths.get(args[0]);
+       search(start, p -> p.toFile().getName().endsWith(args[1])).forEach(System.out::println);
     }
 
-    public static void validate(Path start, String format) {
-        if (start == null && format.length() == 0) {
-            throw new IllegalArgumentException("Root folder is null. Usage  ROOT_FOLDER.");
+    /**
+     * Проверка правильной передачи аргументов
+     * @param args аргумент запуска
+     */
+    private static void validate(String[] args) {
+        if (args.length != 2) {
+            throw new IllegalArgumentException("Не то колличество аргументов");
         }
-    }
+        File file = new File(args[0]);
+        if (!file.exists()) {
+            throw new IllegalArgumentException(String.format("Проверьте, что файл по этому пути существует %s", file.getAbsolutePath()));
+        }
 
+        if (!file.isDirectory()) {
+            throw new IllegalArgumentException(String.format("Проверьте, что путь - это директория, а не файл %s", file.getAbsoluteFile()));
+        }
+
+        if (args[1].length() < 2) {
+            throw new IllegalArgumentException("Введите ещё один аргумент");
+        }
+        if (!args[1].startsWith(".")) {
+            throw new IllegalArgumentException("Неправильно указан путь. Сделайте проверку, что строка начинается с точки");
+        }
+
+    }
     public static List<Path> search(Path root, Predicate<Path> condition) throws IOException {
         SearchFiles searcher = new SearchFiles(condition);
         Files.walkFileTree(root, searcher);
