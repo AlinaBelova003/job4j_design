38c8877562537151a2058fe0815babe40ba68e3d
alina <alinka_belova_2003@mail.ru>
2022-11-14 19:04:58 +0300

    5.2. Архивировать проект [#861 #359603]

--- src/main/java/ru/job4j/io/Zip.java
+++ src/main/java/ru/job4j/io/Zip.java
@@ -6,6 +6,7 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
 import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
 import java.util.zip.ZipOutputStream;
 
 /**
@@ -22,12 +23,23 @@ public class Zip {
     private static String output = "o";
 
     /**
-     * Считывает все файлы из переданного списка в архив.
+     * Считывает ВСЕ файлы из переданного списка в архив.
+     * Прходим по списку и читаем объекты Zip
+     * Добавляем их в path, который читаем и передаём в sours
      * @param source список куда записываем значения из target
      * @param target файл из которого читаем
      */
-    public void packFiles(List<Files> source, File target) {
-
+    public void packFiles(List<Path> source, File target) {
+         try (ZipOutputStream zip = new ZipOutputStream(new BufferedOutputStream(new FileOutputStream(target)))) {
+             for (Path path : source) {
+                 zip.putNextEntry(new ZipEntry(path.toFile().getPath()));
+                 try (BufferedInputStream out = new BufferedInputStream(new FileInputStream(path.toFile()))) {
+                     zip.write(out.readAllBytes());
+                 }
+             }
+         } catch (IOException e) {
+             e.printStackTrace();
+         }
     }
 
     /**
@@ -52,14 +64,14 @@ public class Zip {
         }
     }
     private void validate(ArgsName args) {
-        ArgsName argsName = ArgsName.of(args);
-        if (!argsName.get(directory).isDirectory()) {
+       ArgsName argsName = ArgsName.of(args);
+        if () {
             throw new IllegalArgumentException(String.format("Проверьте, что путь - это директория, а не файл %s", args[0]));
         }
-        if (!args.get(exclude).startsWith(".") && exclude.length() < 2) {
+        if (!argsName.get(exclude).startsWith(".") && exclude.length() < 2) {
             throw new IllegalArgumentException("Аргумент 'e' должен начинаться с точки!");
         }
-        if (args.get(output).endsWith(".zip")) {
+        if (argsName.get(output).endsWith(".zip")) {
             throw new IllegalArgumentException("Аргумент должен иметь расширение .zip");
         }
     }
