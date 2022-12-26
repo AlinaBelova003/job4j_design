5e0e932ccf7a118c5546a618400338a86beacbfd
alina <alinka_belova_2003@mail.ru>
2022-12-26 19:13:38 +0300

    0. Что такое база данных. [#6860]

---
+++ sql/student.sql
@@ -0,0 +1,5 @@
+insert into student(name, age, start_data) values('Alina', 18, '2022-10-11');
+select * from student;												 
+update student set age = 17;
+select * from student;
+delete from student;
