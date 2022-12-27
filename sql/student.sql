c371b290fb876b428b8e6335e73fba98aab914f3
alina <alinka_belova_2003@mail.ru>
2022-12-27 14:34:29 +0300

    0. Что такое база данных. [#6860] Добавлена команда создания таблицы.

--- sql/student.sql
+++ sql/student.sql
@@ -1,5 +1,12 @@
+create table student(
+	id serial primary key,
+	name varchar(50),
+	age int,
+	start_data date
+);
 insert into student(name, age, start_data) values('Alina', 18, '2022-10-11');
 select * from student;												 
 update student set age = 17;
 select * from student;
 delete from student;
+select * from student; 
