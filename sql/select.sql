b152ddbaf4adc5d617a1fd8e55388e96b7d280a5
alina <alinka_belova_2003@mail.ru>
2023-01-15 22:16:00 +0300

    0. Select [#6861 #381024]

---
+++ sql/select.sql
@@ -0,0 +1,17 @@
+create table fauna(
+	id serial primary key,
+	name varchar(255) not null,
+	avg_age int not null,
+	discovery_data date
+);
+
+insert into fauna (name, avg_age, discovery_data) VALUES ('Гиганская черепаха', 29200, '10.09.1885');
+insert into fauna (name, avg_age, discovery_data) VALUES ('Курносая обезьяна', 10300, '10.05.2010');
+insert into fauna (name, avg_age, discovery_data) VALUES ('Утконос', 3652, '05.08.1802');
+insert into fauna (name, avg_age, discovery_data) VALUES ('Морские свинки', 1825, '20.11.1580');
+insert into fauna (name, avg_age, discovery_data) VALUES ('Pike fish', 6570, NULL);
+
+select name FROM fauna where name LIKE '%FISH%';
+select name FROM fauna where avg_age >= 10000 AND avg_age <= 21000;
+select name FROM fauna where discovery_data IS NULL;
+select name FROM fauna where discovery_data < '10.09.1950';
