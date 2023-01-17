19e23ae4bb9e2556d216310bc42e462157b5c889
alina <alinka_belova_2003@mail.ru>
2023-01-17 15:12:20 +0300

    1. Inner join [#373617]

--- sql/many_to_one.sql
+++ sql/many_to_one.sql
@@ -10,8 +10,21 @@ create table order_shop(
 );
 	
 	insert into price_product(price) VALUES (3000);
+	insert into price_product(price) values (30000);
+	insert into price_product(price) values (20000);
+
 	insert into order_shop(name, price_product_id) values ('Камера', 1);
+	insert into order_shop(name, price_product_id) values ('Планшет', 2);
+	insert into order_shop(name, price_product_id) values ('Телефон', 3);
+	insert into order_shop(name) values ('Камера');
+
 	
-	select * from order_shop;
-	
-	select * from price_product where id in (select price_product_id from order_shop);
+	select os.name, pp.price
+	from order_shop as os join price_product as pp on os.price_product_id = pp.id;
+
+	select os.name as Имя, pp.price Цена
+	from order_shop as os join price_product as pp on os.price_product_id = pp.id;
+
+	select os.name "Имя товара", pp.price Цена
+	from order_shop os join price_product pp on os.price_product_id = pp.id;
+
