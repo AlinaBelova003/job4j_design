51fd0eb6326fc49c724967a1349b3fae3bb14f75
alina <alinka_belova_2003@mail.ru>
2023-01-27 00:43:34 +0300

    1. Схема прав пользователи и роли. [#1093 #379882]

--- sql/create.sql
+++ sql/create.sql
@@ -1,6 +1,6 @@
 create table role(
 	id serial primary key,
-	name_role varchar(255),
+	name_role varchar(255)
 );
 
 create table rules (
@@ -15,25 +15,25 @@ create table role_rules(
 );
 create table users (
 	id serial primary key,
-	name varchar(255)
+	name varchar(255),
 	role_id int references role(id)
 );
 
 create table category(
 	id serial primary key,
-	name_category varchar(255),
+	name_category varchar(255)
 );
 
 create table state(
 	id serial primary key,
-	name_state varchar(255),
+	name_state varchar(255)
 );
 
 create table item(
 	id serial primary key,
 	name_item varchar(255),
-	users_id int references users(id)
-	category_id int references category(id)
+	users_id int references users(id),
+	category_id int references category(id),
 	state_id int references state(id)
 );
 
