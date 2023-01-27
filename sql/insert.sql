675c1e1438b55ad6a92e8818f02e14c7316f2840
alina <alinka_belova_2003@mail.ru>
2023-01-27 13:25:30 +0300

    1. Схема прав пользователи и роли. [#1093 #379882]

--- sql/insert.sql
+++ sql/insert.sql
@@ -1,8 +1,5 @@
-insert into users(name) VALUES ('Alina');
-insert into users(name) VALUES ('Ivan');
-
-insert into role(name_role, users_id) VALUES ('Castomer1', 1);
-insert into role(name_role) VALUES ('Castomer2', 2);
+insert into role(name_role) VALUES ('Castomer1');
+insert into role(name_role) VALUES ('Castomer2');
 
 insert into rules(name_rules) VALUES ('freeze');
 insert into rules(name_rules) VALUES ('do not throw');
@@ -10,6 +7,9 @@ insert into rules(name_rules) VALUES ('do not throw');
 insert into role_rules(role_id, rules_id) VALUES (1, 2);
 insert into role_rules(role_id, rules_id) VALUES (1, 1);
 insert into role_rules(role_id, rules_id) VALUES (1, 2);
+
+insert into users(name, role_id) VALUES ('Alina', 1);
+insert into users(name, role_id) VALUES ('Ivan', 2);
 
 insert into item(name_item, users_id, name_state_id, name_category_id) Values ('paint', 1, 1, 1);
 insert into item(name_item, users_id, name_state_id, name_category_id) Values ('crystals', 2, 2, 2);
